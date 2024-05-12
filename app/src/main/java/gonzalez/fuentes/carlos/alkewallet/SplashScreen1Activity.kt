7fdc4193b041a5ad889ef8e60a87415562c915a0
Carlos-Alberto-Antonio-G-Fuentes <carlangas0273@gmail.com>
2024-05-11 20:09:38 -0400

    Creacion del modelo

--- app/src/main/java/gonzalez/fuentes/carlos/alkewallet/SplashScreen1Activity.kt
+++ app/src/main/java/gonzalez/fuentes/carlos/alkewallet/SplashScreen1Activity.kt
@@ -2,20 +2,24 @@ package gonzalez.fuentes.carlos.alkewallet
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import gonzalez.fuentes.carlos.alkewallet.databinding.ActivitySplashScreen1Binding
 
 class SplashScreen1Activity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashScreen1Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen1)
+        binding= ActivitySplashScreen1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         //Declarar imagen en una variable
-        val logoapp=findViewById<ImageView>(R.id.imagelogo)
-        logoapp.setOnClickListener{
+        binding.imagelogo.setOnClickListener{
             val intent= Intent(this,LoginSignupPage2Activity::class.java)
             startActivity(intent)
         }
